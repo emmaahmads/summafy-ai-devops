03c2e9d706bbdfaaff0afd64b227fe0f3e5c1345
emmaahmads <emmaahmadsir@gmail.com>
2025-05-13 02:04:37 +0800

    terraform cd

--- terraform/statemachine.tf
+++ terraform/statemachine.tf
@@ -1,7 +1,7 @@
-variable "bucket_name" {
-  description = "The name of the S3 bucket to use as the source bucket."
-  type        = string
-}
+# variable "bucket_name" {
+#   description = "The name of the S3 bucket to use as the source bucket."
+#   type        = string
+# }
 
 data "aws_s3_bucket" "source_bucket" {
   bucket = "superumi-summafy-123"
